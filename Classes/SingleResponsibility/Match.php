6edec7cc3631fedab0850bb3d0ffbe682d51f951
Osama arafa <osamaarafa15@gmail.com>
2020-01-29 21:32:19 +0200

    Wrong Single Responsibility Principle

---
+++ Classes/SingleResponsibility/Match.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Classes\SingleResponsibility;
+
+
+class Match
+{
+
+    public function attack(){
+        
+    }
+
+    public function defense(){
+        
+    }
+
+    public function keeper(){
+
+    }
+}
